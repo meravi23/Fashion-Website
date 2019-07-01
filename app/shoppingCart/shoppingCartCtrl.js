f0fdcfbbe049fed882a2db36fd5b77de7f1447b0
annabelo2606 <anna.belo@gmail.com>
2019-07-01 10:38:40 +0300

    shopping cart calculations

--- app/shoppingCart/shoppingCartCtrl.js
+++ app/shoppingCart/shoppingCartCtrl.js
@@ -8,30 +8,35 @@ app.controller("shoppingCartCtrl", function($scope, $location, userSrv, productS
 
     shoppingCartSrv.getShoppingCartPerUserID().then(function(shoppingCarts) {
         $scope.shoppingCarts = shoppingCarts;
-        recalculateCart();
+        $scope.recalculateCart();
     }, function(err) {
         console.error(err);
     });
 
-    $scope.removeItemFromCart = function(shoppingCart) {
-        recalculateCart();
-    }
-
-    $scope.updateProductQuantityInCart = function(shoppingCart, productQuantity) {
-        shoppingCartSrv.updateShoppingCartQuantity(shoppingCart, productQuantity).then(function(shoppingCarts) {
+    $scope.removeProductfromCart = function(shoppingCart) {
+        userSrv.removeProductfromCart(shoppingCart).then(function(shoppingCarts) {
             $scope.shoppingCarts = shoppingCarts;
-            recalculateCart();
+            $scope.recalculateCart();
         }, function(err) {
             console.error(err);
         });
     }
 
-    function recalculateCart() {
+    $scope.updateProductQuantityInCart = function(shoppingCart) {
+        shoppingCartSrv.updateShoppingCartQuantity(shoppingCart, productQuantity).then(function(shoppingCarts) {
+            $scope.shoppingCarts = shoppingCarts;
+            $scope.recalculateCart();
+        }, function(err) {
+            console.error(err);
+        });
+    }
+
+    $scope.recalculateCart = function() {
         $scope.subtotal = 0;
         if ($scope.shoppingCarts.length > 0) {
             $shoppingCartIsEmpty = true;
             for (var i = 0; i < $scope.shoppingCarts.length; i++) {
-                $scope.subtotal = $scope.shoppingCarts[i].linePrice;
+                $scope.subtotal += $scope.shoppingCarts[i].productPrice * $scope.shoppingCarts[i].productQuantity;
             }
         }
         return $scope.subtotal;
